2e2d8a843ae3cc6af4199d26eaa29e6e2744b9f2
googol88 <ronaldfleung@gmail.com>
2023-03-15 21:29:56 -0400

    🎨 feat: use Image component

--- components/Card.js
+++ components/Card.js
@@ -1,24 +1,36 @@
-// react component
 import React from 'react'
-// basic react component
+import Image from 'next/image'
+
 export default function Card() {
   return (
-    <div className="bg-slate-100 border rounded-xl p-8 m-4 shadow-lg">
-      <img className="w-24 h-24 rounded-full mx-auto" src="https://avatars.githubusercontent.com/u/57511576?v=4" alt="profile picture" width="384" height="512"></img>
-      <div className="pt-6 text-center space-y-4">
+    <div className="p-8 m-4 border shadow-lg bg-slate-100 rounded-xl">
+
+      <a
+        href="https://github.com/googol88"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image className="w-24 h-24 mx-auto border rounded-full shadow-md" src="https://avatars.githubusercontent.com/u/57511576?v=4" alt="profile" width={384} height={384} />
+      </a>
+      <div className="pt-6 text-center">
         <blockquote>
           <p className="text-lg font-medium">
             Hello world haha this is so funny
           </p>
         </blockquote>
-        <figcaption className="font-medium">
+        <a
+          href="https://github.com/googol88"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mt-4 font-medium"
+        >
           <div className="text-sky-500 dark:text-sky-400">
             Googol
           </div>
           <div className="text-slate-700 dark:text-slate-500">
             @googol88
           </div>
-        </figcaption>
+        </a>
       </div>
     </div>
   )
--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/u/**',
+      },
+    ],
+  },
 }
